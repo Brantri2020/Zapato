0ae5ff1081cf31049c2169029e5afa8ed9f33dab
Brandon <brandon.toapanta@epn.edu.ec>
2021-10-18 11:42:42 -0500

    Routing

--- app-empleados/src/app/app.component.ts
+++ app-empleados/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Empleado } from './empleado.model';
-import { EmpleadosService } from './empleados.service';
-import { ServicioEmpleadosService } from './servicio-empleados.service';
+
 
 @Component({
   selector: 'app-root',
@@ -9,30 +7,14 @@ import { ServicioEmpleadosService } from './servicio-empleados.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  titulo = 'Listado empleados';
 
-  constructor(private miServicio:ServicioEmpleadosService, private empleadosService:EmpleadosService){
-
-    //this.empleados=this.empleadosService.empleados;
+  constructor(){
 
   }
   ngOnInit(): void {
-    this.empleados=this.empleadosService.empleados;
+   
   }
 
-  empleados: Empleado[] = [];
-
-  agregarEmpleado() {
-    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
-    //this.miServicio.muestraMensaje("Nombre del empleado: "+miEmpleado.nombre);
-    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
-
-  }
-
-
-  cuadroNombre: string = "";
-  cuadroApellido: string = "";
-  cuadroCargo: string = "";
-  cuadroSalario: number = 0;
+ 
 
 }
