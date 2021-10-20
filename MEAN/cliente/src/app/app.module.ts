e4058be4bafeb06ae9bb59bf753cdc9844b5c3ef
Brandon <brandon.toapanta@epn.edu.ec>
2021-10-20 12:29:45 -0500

    MEAN 2 Formularios

--- MEAN/cliente/src/app/app.module.ts
+++ MEAN/cliente/src/app/app.module.ts
@@ -2,19 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+// componentes
+
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { QuitarProductosComponent } from './components/quitar-productos/quitar-productos.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CrearProductoComponent,
-    QuitarProductosComponent
+    QuitarProductosComponent,
+    ListarProductosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
