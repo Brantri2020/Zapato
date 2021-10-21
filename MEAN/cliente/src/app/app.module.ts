15fef340d54cbe3f2d84091c2cc94463dc9a9f75
Brandon <brandon.toapanta@epn.edu.ec>
2021-10-21 10:55:04 -0500

    MEAN metodos base datos

--- MEAN/cliente/src/app/app.module.ts
+++ MEAN/cliente/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
+
 // componentes
 
 import { AppComponent } from './app.component';
--- MEAN/servidor/index.js
+++ MEAN/servidor/index.js
@@ -8,12 +8,10 @@ const app = express();
 //Conectamos a la BD
 conectarDB();
 
+app.use(express.json());
+
 app.use('/api/productos', require('./routes/producto'));
 
-//Definimos ruta principal
-/*app.get('/', (req, res)=>{
-    res.send('Hola mundo');
-})*/
 
 app.listen(4000, () =>{
     console.log('El servidor esta corriendo perfectamente');
